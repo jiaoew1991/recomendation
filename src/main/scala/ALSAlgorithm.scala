067482dbcfed05f6046c666dd597ae83236be36a
jiaoew <jiaoew2011@gmail.com>
2015-01-31 13:53:42 +0800

    reset algorithm params

--- src/main/scala/ALSAlgorithm.scala
+++ src/main/scala/ALSAlgorithm.scala
@@ -1,18 +1,17 @@
-import breeze.linalg.rank
 import grizzled.slf4j.Logger
 import io.prediction.controller.{IPersistentModel, IPersistentModelLoader, PAlgorithm, Params}
 import io.prediction.data.storage.BiMap
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
-import org.apache.spark.mllib.recommendation.{Rating => MLlibRating, MatrixFactorizationModel, ALS}
+import org.apache.spark.mllib.recommendation.{ALS, Rating => MLlibRating}
 import org.apache.spark.rdd.RDD
 
 import scala.collection.mutable.PriorityQueue
 
-case class ALSAlgorithmParams(rank: Int, numIterations: Int) extends Params
+case class ALSAlgorithmParams(rank: Int, numIterations: Int, lambda: Double) extends Params
 
 class ALSModel(
-                val matrix: MatrixFactorizationModel,
+                val productFeatures: RDD[(Int, Array[Double])],
                 val itemStringIntMap: BiMap[String, Int],
                 val items: Map[Int, Item]
                 ) extends IPersistentModel[ALSAlgorithmParams] with Serializable {
@@ -20,19 +19,15 @@ class ALSModel(
   @transient lazy val itemIntStringMap = itemStringIntMap.inverse
 
   def save(id: String, params: ALSAlgorithmParams, sc: SparkContext): Boolean = {
-    sc.parallelize(Seq(rank)).saveAsObjectFile(s"/tmp/${id}/rank")
-    matrix.userFeatures.saveAsObjectFile(s"/tmp/${id}/userFeatures")
-    matrix.productFeatures.saveAsObjectFile(s"/tmp/${id}/productFeatures")
+    productFeatures.saveAsObjectFile(s"/tmp/${id}/productFeatures")
     sc.parallelize(Seq(itemStringIntMap)).saveAsObjectFile(s"/tmp/${id}/itemStringIntMap")
     sc.parallelize(Seq(items)).saveAsObjectFile(s"/tmp/${id}/items")
     true
   }
 
   override def toString = {
-    s" rank: ${matrix.rank}" +
-      s" userFeatures: [${matrix.userFeatures.count()}}]" +
-      s" productFeatures: [${matrix.productFeatures.count()}]" +
-      s"(${matrix.productFeatures.take(2).toList}...)" +
+    s" productFeatures: [${productFeatures.count()}]" +
+      s"(${productFeatures.take(2).toList}...)" +
       s" itemStringIntMap: [${itemStringIntMap.size}]" +
       s"(${itemStringIntMap.take(2).toString}...)]" +
       s" items: [${items.size}]" +
@@ -43,11 +38,7 @@ class ALSModel(
 object ALSModel extends IPersistentModelLoader[ALSAlgorithmParams, ALSModel] {
   def apply(id: String, params: ALSAlgorithmParams, sc: Option[SparkContext]) = {
     new ALSModel(
-      new MatrixFactorizationModel(
-        rank = sc.get.objectFile[Int](s"/tmp/${id}/rank").first,
-        userFeatures = sc.get.objectFile(s"/tmp/${id}/userFeatures"),
-        productFeatures = sc.get.objectFile(s"/tmp/${id}/productFeatures")
-      ),
+      productFeatures = sc.get.objectFile(s"/tmp/${id}/productFeatures"),
       itemStringIntMap = sc.get.objectFile[BiMap[String, Int]](s"/tmp/${id}/itemStringIntMap").first,
       items = sc.get.objectFile[Map[Int, Item]](s"/tmp/${id}/items").first)
   }
@@ -99,6 +90,7 @@ class ALSAlgorithm(val ap: ALSAlgorithmParams) extends PAlgorithm[PreparedData, 
 
       ((uindex, iindex), r.score)
     }.filter { case ((u, i), v) =>
+      // keep events with valid user and item index
       (u != -1) && (i != -1)
     }.reduceByKey(_ + _) // aggregate all view events of same user-item pair
       .map { case ((u, i), v) =>
@@ -113,7 +105,11 @@ class ALSAlgorithm(val ap: ALSAlgorithmParams) extends PAlgorithm[PreparedData, 
     val m = ALS.trainImplicit(mllibRatings, ap.rank, ap.numIterations)
     //    m.recommendProducts()
 
-    new ALSModel(m, itemStringIntMap, items)
+    new ALSModel(
+      productFeatures = m.productFeatures,
+      itemStringIntMap = itemStringIntMap,
+      items = items
+    )
   }
 
   def predict(model: ALSModel, query: Query): PredictedResult = {
@@ -123,7 +119,7 @@ class ALSAlgorithm(val ap: ALSAlgorithmParams) extends PAlgorithm[PreparedData, 
       .flatten.toSet
 
     val queryFeatures: Vector[Array[Double]] = queryList.toVector.par
-      .map { item => model.matrix.productFeatures.lookup(item).head}.seq
+      .map { item => model.productFeatures.lookup(item).head}.seq
 
     val whiteList: Option[Set[Int]] = query.whiteList.map(set =>
       set.map(model.itemStringIntMap.get(_)).flatten
@@ -134,11 +130,11 @@ class ALSAlgorithm(val ap: ALSAlgorithmParams) extends PAlgorithm[PreparedData, 
 
     val ord = Ordering.by[(Int, Double), Double](_._2).reverse
 
-    val indexScores = if (queryFeatures.isEmpty) {
+    val indexScores: Array[(Int, Double)] = if (queryFeatures.isEmpty) {
       logger.info(s"No valid items in ${query.items}.")
       Array[(Int, Double)]()
     } else {
-      model.matrix.productFeatures.mapValues { f =>
+      model.productFeatures.mapValues { f =>
         queryFeatures.map(qf => cosine(qf, f)).reduce(_ + _)
       }.collect()
     }
