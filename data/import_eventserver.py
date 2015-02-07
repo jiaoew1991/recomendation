be79456a4fe0d71db5ba111b867a0e61cb5de6e0
jiaoew <jiaoew2011@gmail.com>
2015-02-07 20:16:18 +0800

    fix import data bug

--- data/import_eventserver.py
+++ data/import_eventserver.py
@@ -9,6 +9,7 @@ import os
 
 SEED = 3
 
+
 def import_events(client, dir):
     random.seed(SEED)
     count = 0
@@ -18,7 +19,7 @@ def import_events(client, dir):
     # generate 10 users, with user ids u1,u2,....,u10
     with open(os.path.join(dir, "user.txt"), 'r') as user_file:
         for user_line in user_file:
-            user_data = user_line.rstrip('\r\n').split('\t')
+            user_data = user_line.rstrip().split('\t')
             user_id = user_data[0]
             print "Set user", user_id
             client.create_event(
@@ -31,17 +32,17 @@ def import_events(client, dir):
                 entity_type="item",
                 entity_id=user_id,
                 properties={
-                    "profiles": user_data[1:]
+                    "profiles": map(float, user_data[1:])
                 }
             )
             count += 1
 
     with open(os.path.join(dir, 'like.txt'), 'r') as like_file:
         for like_line in like_file:
-            like_data = like_line.rstrip('\r\n').split('\t')
+            like_data = like_line.rstrip().split('\t')
             user_id = like_data[0]
             item_id = like_data[1]
-            print "User", user_id ,"like item", item_id
+            print "User", user_id, "like item", item_id
             client.create_event(
                 event="like",
                 entity_type="user",
@@ -52,6 +53,7 @@ def import_events(client, dir):
             count += 1
 
     print "%s events are imported." % count
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
